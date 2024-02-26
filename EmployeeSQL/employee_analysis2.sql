ab37089d34b7d38199e0f32b670c4b0904ec7730
larsenamy14 <larsenamy14@gmail.com>
2024-02-25 19:57:35 -0800

    Changed data type of hire_date and updated code to find employees that were hired in 1986

--- EmployeeSQL/employee_analysis2.sql
+++ EmployeeSQL/employee_analysis2.sql
@@ -7,13 +7,7 @@ ON e.emp_no = s.emp_no;
 --List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE emp_no IN 
-	(
-	SELECT emp_no
-	FROM employees
-	WHERE 
-	RIGHT(hire_date, 4) = '1986'
-	);
+WHERE YEAR hire_date = 1986;
 	
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
 SELECT e.first_name, e.last_name, dm.dept_no, d.dept_name, de.emp_no 
